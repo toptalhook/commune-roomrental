318c96b6d7bfa3eebf735d5c6d5f893b0992a392
toptalhook <toptal.cook@gmail.com>
2024-05-06 09:14:19 +0900

    feat: add ethers provider.

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,8 +1,11 @@
 "use client";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import {SessionProvider} from 'next-auth/react'
+
+import { isWallectConnected, loadAppartments } from '@/Blockchain.services';
+import { setGlobalState, useGlobalState, getGlobalState } from "@/store";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,6 +16,16 @@ const queryClient = new QueryClient({
 });
 
 const Providers = ({ children }: PropsWithChildren) => {
+  const [ connectedAccount ] = useGlobalState('connectedAccount');
+
+  useEffect( () => {
+    const service = async () => {
+    await isWallectConnected();
+    await loadAppartments(); 
+    console.log(getGlobalState('appartments'))
+  }
+  service();
+  }, [connectedAccount]);
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>
